5733da2c67f06af29cacf64564d79f8e88035d6b
Mark Markarian <mark@interzonedev.com>
2013-01-19 00:06:09 -0800

    Added the ability to checkout the OddJob git repo.

--- src/main/java/com/interzonedev/oddjobbuilder/service/BuilderServiceImpl.java
+++ src/main/java/com/interzonedev/oddjobbuilder/service/BuilderServiceImpl.java
@@ -6,6 +6,11 @@ import java.io.IOException;
 import javax.annotation.PostConstruct;
 import javax.inject.Named;
 
+import org.eclipse.jgit.api.CloneCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.TransportException;
 import org.slf4j.LoggerFactory;
 
 import ch.qos.logback.classic.Logger;
@@ -19,6 +24,31 @@ public class BuilderServiceImpl implements BuilderService {
 
 	private final String tempDirPath = userDir + "/tmp";
 
+	private static final String REPO_URL = "git://github.com/interzonedev/OddJob.git";
+
+	private static final String REPO_TAG = "v1.2.0";
+
+	private class WorkDirectories {
+
+		private final String repoDirectoryPath;
+
+		private final String buildDirectoryPath;
+
+		public WorkDirectories(String repoDirectoryPath, String buildDirectoryPath) {
+			this.repoDirectoryPath = repoDirectoryPath;
+			this.buildDirectoryPath = buildDirectoryPath;
+		}
+
+		public String getRepoDirectoryPath() {
+			return repoDirectoryPath;
+		}
+
+		public String getBuildDirectoryPath() {
+			return buildDirectoryPath;
+		}
+
+	}
+
 	@PostConstruct
 	public void init() {
 		log.debug("init: userDir = " + userDir);
@@ -29,7 +59,9 @@ public class BuilderServiceImpl implements BuilderService {
 
 		log.debug("buildLibrary: Start");
 
-		initDirectories();
+		WorkDirectories workDirectories = initDirectories();
+
+		cloneRepo(workDirectories.getRepoDirectoryPath());
 
 		log.debug("buildLibrary: End");
 
@@ -37,17 +69,40 @@ public class BuilderServiceImpl implements BuilderService {
 
 	}
 
-	private void initDirectories() throws IOException {
+	private void cloneRepo(String repoDirectoryPath) throws InvalidRemoteException, TransportException, GitAPIException {
+
+		File repoDirectory = new File(repoDirectoryPath);
+
+		if (repoDirectory.listFiles().length != 0) {
+			log.debug("cloneRepo: " + repoDirectory + " is not empty");
+			return;
+		}
+
+		log.debug("cloneRepo: Cloning repo from " + REPO_URL);
+
+		CloneCommand cloneCommand = new CloneCommand();
+
+		Git git = cloneCommand.setURI(REPO_URL).setDirectory(repoDirectory).setBranch("master").call();
+
+		git.checkout().setName(REPO_TAG).call();
+
+		log.debug("cloneRepo: Cloned repo into " + repoDirectory + " and checked out " + REPO_TAG);
+
+	}
+
+	private WorkDirectories initDirectories() throws IOException {
 
 		String tempDirCanonicalPath = makeDirectory(tempDirPath);
 
 		log.debug("Using temp directory: " + tempDirCanonicalPath);
 
 		String repoDirPath = tempDirCanonicalPath + "/repo";
-		makeDirectory(repoDirPath);
+		String repoDirCanonicalPath = makeDirectory(repoDirPath);
 
 		String buildDirPath = tempDirCanonicalPath + "/build";
-		makeDirectory(buildDirPath);
+		String buildDirCanonicalPath = makeDirectory(buildDirPath);
+
+		return new WorkDirectories(repoDirCanonicalPath, buildDirCanonicalPath);
 
 	}
 
